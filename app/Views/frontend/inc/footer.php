3598a33986055daabd17e0e1333f8168e4399e8e
Nisam Git <nisamvp10@gmail.com>
2026-04-24 10:54:29 +0530

    footer

--- app/Views/frontend/inc/footer.php
+++ app/Views/frontend/inc/footer.php
@@ -1,141 +1,146 @@
 
+<footer class="footer-section container ">
+                    <div class="footer-area main-footer bg-section dark-section">
+                        <div class="container-fluid">
+                            <div class="footer-widget-wrapper">
+                                <div class="row justify-content-between">
+                                    <div class="col-xl-3 col-lg-5 col-md-6 wow fadeInUp" data-wow-delay=".2s" style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInUp;">
+                                        <div class="footer-widget-items">
+                                            <div class="widget-head">
+                                                <a href="index.html" class="footer-logo">
+                                                    <img src="<?=base_url('public/assets/template/images/footlogo.jpeg')?>" alt="img">
+                                                </a>
+                                            </div>
+                                            <div class="footer-content">
+                                                <p>
+                                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+                                                </p>
+                                               <div class="footer-social-links">
+                                                    <ul>
+                                                        <li><a href="#"><i class="fa-brands fa-pinterest-p"></i></a></li>
+                                                        <li><a href="#"><i class="fa-brands fa-x-twitter"></i></a></li>
+                                                        <li><a href="#"><i class="fa-brands fa-facebook-f"></i></a></li>
+                                                        <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
+                                                    </ul>
+                                                </div>
 
-    <!-- Footer Main Start -->
-    <footer class="main-footer bg-section dark-section">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <!-- Footer Header Start -->
-                    <div class="footer-header">
-                        <!-- Section Title Start -->
-                        <div class="section-title footer-newsletter-title">
-                            <h2 class="text-anime-style-3" data-cursor="-opaque">Book Your Appointment Now</h2>
-                        </div>
-                        <!-- Section Title End -->
-
-                        <!-- Footer Newsletter Form Start -->
-                        <div class="footer-newsletter-form">
-                            <form id="newslettersForm" action="#" method="POST">
-                                <div class="form-group">
-                                    <input type="email" name="mail" class="form-control"  id="mail" placeholder="Enter your email" required>
-                                    <button type="submit" class="readmore-btn"><img src="images/arrow-white.svg" alt=""></button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-xl-2 col-lg-3 col-md-6 wow fadeInUp" data-wow-delay=".4s" style="visibility: visible; animation-delay: 0.4s; animation-name: fadeInUp;">
+                                        <div class="footer-widget-items footer-menu">
+                                            <div class="widget-head">
+                                                <h3>Useful Links</h3>
+                                            </div>
+                                            <ul class="gt-list-area">
+                                                <li>
+                                                    <a href="<?=base_url()?>">
+                                                        Home
+                                                    </a>
+                                                </li>
+                                                <li>
+                                                    <a href="<?=base_url('profile')?>">
+                                                        Profile
+                                                    </a>
+                                                </li>
+                                                <li>
+                                                    <a href="<?=base_url('purpose-and-values')?>">
+                                                        Purpose and Values
+                                                    </a>
+                                                </li>
+                                                <li>
+                                                    <a href="<?=base_url('contact')?>">
+                                                        Contact Us
+                                                    </a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="col-xl-2 ps-lg-0 col-lg-4 col-md-6 wow fadeInUp" data-wow-delay=".6s" style="visibility: visible; animation-delay: 0.6s; animation-name: fadeInUp;">
+                                        <div class="footer-widget-items footer-menu">
+                                            <div class="widget-head">
+                                                <h3>Services</h3>
+                                            </div>
+                                            <ul class="gt-list-area">
+                                                <li>
+                                                    <a href="programmes.html">
+                                                        Diploma Courses
+                                                    </a>
+                                                </li>
+                                                 <?php
+                                                    if(services()){
+                                                        foreach(services() as $navService) {
+                                                    ?>
+                                                        <li><a href="<?=base_url("services/".$navService["slug"]);?>"> <?=$navService["category"];?> </a></li>
+                                                    <?php
+                                                        }
+                                                    }    ?>       
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="col-xl-2 col-lg-4 col-md-6 wow fadeInUp" data-wow-delay=".8s" style="visibility: visible; animation-delay: 0.8s; animation-name: fadeInUp;">
+                                        <div class="footer-widget-items footer-menu">
+                                            <div class="widget-head">
+                                                <h3>Phone</h3>
+                                            </div>
+                                            <ul class="gt-list-area">
+                                                <li>
+                                                    <img src="<?=base_url("public/assets/template/images/icon-phone.svg");?>"> (+91) 812 939 4980
+                                                </li>
+                                                <li>
+                                                    <img src="<?=base_url("public/assets/template/images/icon-phone.svg");?>"> (+91) 812 939 4980
+                                                </li>
+                                                <!-- <li>
+                                                    <img src="assets/img/home-main/country-3.svg"> +91 800 800 55 38
+                                                </li>
+                                                <li>
+                                                    <img src="assets/img/home-main/country-4.svg"> +974 502 777 18
+                                                </li> -->
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="col-xl-3 col-lg-7 col-md-12 wow fadeInUp" data-wow-delay=".8s" style="visibility: visible; animation-delay: 0.8s; animation-name: fadeInUp;">
+                                        <div class="footer-widget-items">
+                                            <div class="widget-head">
+                                                <h3>Our Office Locations</h3>
+                                            </div>
+                                            <ul class="contact-list footer-address">
+                                               <li>
+                                                    Ma Care Polyclinic & Diagnostic Centre 
+High school Junction, Valapad, Thrissur, Kerala 680567
+                                               </li>
+                                               
+                                            </ul>
+                                            <ul class="contact-list-address">
+                                                <li>
+                                                    M / drsudhanofficial.com | info@joinfactory.org
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
                                 </div>
-                            </form>
+                            </div>
                         </div>
-                        <!-- Footer Newsletter Form End -->
+                        <div class="footer-bottom">
+                            <div class="container-fluid">
+                                <div class="footer-bottom-wrapper">
+                                    <p class="wow fadeInUp" data-wow-delay=".3s" style="visibility: visible; animation-delay: 0.3s; animation-name: fadeInUp;">
+                                        © 2025 <b>Jointfactory</b> All rights reserved.
+                                    </p>
+
+                                    <ul class="footer-list wow fadeInUp" data-wow-delay=".7s" style="visibility: visible; animation-delay: 0.7s; animation-name: fadeInUp;">
+                                        <li>
+                                            <a href="#">Privacy policy</a>
+                                        </li>
+                                        <li>।</li>
+                                        <li>
+                                            <a href="#">Terms &amp; conditions</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <!-- Footer Header End -->                 
-                </div>
+                </footer>
 
-                <div class="col-lg-4 col-md-12">
-                    <!-- About Footer start -->
-                    <div class="about-footer">
-                        <!-- Footer Logo Start -->
-                        <div class="footer-logo">
-                            <img src="images/logof.png" alt="">
-                        </div>
-                        <!-- Footer Logo End -->
-
-                        <!-- About Footer Content Start -->
-                        <div class="about-footer-content">
-                            <p>Have questions or need expert orthopedic care?</p>
-                        </div>
-                        <!-- About Footer Content End -->
-
-                        <!-- Footer Social Link Start -->
-                        <div class="footer-social-links">
-                            <ul>
-                                <li><a href="#"><i class="fa-brands fa-pinterest-p"></i></a></li>
-                                <li><a href="#"><i class="fa-brands fa-x-twitter"></i></a></li>
-                                <li><a href="#"><i class="fa-brands fa-facebook-f"></i></a></li>
-                                <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
-                            </ul>
-                        </div>
-                        <!-- Footer Social Link End -->
-                    </div>
-                    <!-- About Footer End -->
-                </div>
-
-                <div class="col-lg-8">
-                    <div class="footer-links-box">
-                        <!-- Footer Links Start -->
-                        <div class="footer-links quick-links">
-                            <h3>quick link</h3>
-                            <ul>
-                                <li><a href="#">Home</a></li>
-                                <li><a href="#">About us</a></li>
-                                <li><a href="#">services</a></li>
-                                <li><a href="#">Contact us</a></li>
-                            </ul>
-                        </div>
-                        <!-- Footer Links End -->
-
-                        <!-- Footer Links Start -->
-                        <div class="footer-links">
-                            <h3>Open Hours:</h3>
-                            <ul>
-                                <li>Mon-Sat: 10:00 to 07:00</li>
-                                <li>Saturday-Closed</li>
-                            </ul>
-                        </div>
-                        <!-- Footer Links End -->
-                    </div>
-                    
-                    <!-- Footer Contact Box Start -->
-                    <div class="footer-contact-box">
-                        <!-- Footer Contact Item Start -->
-                        <div class="footer-links footer-contact-item">
-                            <h3>Contact:</h3>
-                            <ul>
-                                <li><i class="fa-solid fa-phone"></i><a href="#">(+91) 812 939 4980</a></li>
-                            </ul>
-                        </div>
-                        <!-- Footer Contact Item End -->
-
-                        <!-- Footer Contact Item Start -->
-                        <div class="footer-links footer-contact-item">
-                            <h3>E-mail:</h3>
-                            <ul>
-                                <li><i class="fa-solid fa-envelope"></i><a href="#">info@drsudhanofficial.com</a></li>
-                            </ul>
-                        </div>
-                        <!-- Footer Contact Item End -->
-
-                        <!-- Footer Contact Item Start -->
-                        <div class="footer-links footer-contact-item">
-                            <h3>Address:</h3>
-                            <ul>
-                                <li><i class="fa-solid fa-location-dot"></i>
-                                    High school Junction, Valapad,  Kerala </li>
-                            </ul>
-                        </div>
-                        <!-- Footer Contact Item End -->
-                    </div>
-                    <!-- Footer Contact Box End -->
-                </div>
-
-                <div class="col-lg-12">
-                    <!-- Footer Copyright Section Start -->
-                    <div class="footer-copyright">
-                        <!-- Footer Copyright Text Start -->
-                        <div class="footer-copyright-text">
-                            <p>Copyright ©  Dr. Ram Sudhan Subramaniyan  All Rights Reserved.</p>
-                        </div>
-                        <!-- Footer Copyright Text End -->
-
-                        <!-- Footer Privacy Policy Start -->
-                        <div class="footer-privacy-policy">
-                            <ul>
-                                <li><a href="#">Privacy policy</a></li>
-                                <li><a href="#">Term's & condition</a></li>
-                                <li><a href="#">help</a></li>
-                            </ul>
-                        </div>
-                        <!-- Footer Privacy Policy End -->
-                    </div>
-                    <!-- Footer Copyright Section End -->
-                </div>
-            </div>
-        </div>
-    </footer>
-    <!-- Footer Main End -->
+ 
