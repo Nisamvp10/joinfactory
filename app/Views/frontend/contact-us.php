8fdafeff3bd8c3d041b86d14fa0e79c4fb05e8bf
Nisam Git <nisamvp10@gmail.com>
2026-06-21 15:56:52 +0530

    new contact us page

--- app/Views/frontend/contact-us.php
+++ app/Views/frontend/contact-us.php
@@ -32,98 +32,19 @@
 
 
     <!-- Page Contact Us Start -->
-    <div class="page-contact-us__ main-section">
+    <div class="page-contact-us__ contact-section main-section ">
         <div class="container">
-            <div class="inner-section bg-light-gray">
-            <div class="row">
-                <div class="col-lg-4">
-                    <!-- Contact Us Content Start -->
-                    <div class="contact-us-content">
-                        <!-- Section Title Start -->
+            <div class="inner-section align-items-center p-lg-5 p-md-2 bg-white">
+                <div class="row d-flex align-items-center">
+                    <div class="col-lg-7 col-md-6 col-sm-12">
                         <div class="section-title">
-                            <h2 class="text-anime-style-3" data-cursor="-opaque">Get in Touch – We're Here to Help!
-                            </h2>
+                            <h3 class="text-anime-style-3" data-cursor="-opaque">Get in Touch – We're Here to Help!</h3>
                             <p class="wow fadeInUp" data-wow-delay="0.2s">
                                 Dr. Ram Sudhan Subramaniyan is a highly respected expert in joint replacement, sports injuries, and musculoskeletal care. Specializing in knee, hip, and shoulder replacement improve their quality of life.
-
-
                             </p>
                         </div>
-                        <!-- Section Title End -->
-                    </div>
-                    <!-- Contact Us Content End -->
-                </div>
-
-                <div class="col-lg-8">
-                    <!-- Contact Info List Start -->
-                    <div class="contact-info-list">
-                        <!-- Contact Info Item Start -->
-                        <div class="contact-info-item wow fadeInUp" data-wow-delay="0.4s">
-                            <div class="icon-box">
-                                <img src="<?= base_url('public/assets/template/images/icon-location.svg')?>" alt="">
-                            </div>
-                            <div class="contact-info-content">
-                                <h3>location</h3>
-                                <p>
-                                    9/38, Pulinchode, PO, Potta - Moonupeedika Rd, near pulinchode bus stop, Pullur, Irinjalakuda, Kerala 680683
-
-
-                                </p>
-                            </div>
-                        </div>
-                        <!-- Contact Info Item End -->
-                         
-                        <!-- Contact Info Item Start -->
-                        <div class="contact-info-item wow fadeInUp" data-wow-delay="0.6s">
-                            <div class="icon-box">
-                                <img src="<?= base_url('public/assets/template/images/icon-phone.svg')?>" alt="">
-                            </div>
-                            <div class="contact-info-content">
-                                <h3>contact us</h3>
-                                <p><a href="#">(+91) 812 939 4980
-
-                                </a></p>
-                            </div>
-                        </div>
-                        <!-- Contact Info Item End -->
-
-                        <!-- Contact Info Item Start -->
-                        <div class="contact-info-item wow fadeInUp" data-wow-delay="0.8s">
-                            <div class="icon-box">
-                                <img src="<?= base_url('public/assets/template/images/icon-mail.svg')?>" alt="">
-                            </div>
-                            <div class="contact-info-content">
-                                <h3>email</h3>
-                                <p><a href="#">drsudhanofficial.com
-
-                                </a></p>
-                            </div>
-                        </div>
-                        <!-- Contact Info Item End -->
-                    </div>
-                    <!-- Contact Info List End -->
-                </div>
-            </div>
-            </div>
-        </div>
-    </div>
-    <!-- Page Contact Us End -->
-
-    <!-- Contact Form Start -->
-    <div class="conatct-us-form section-division p-0">
-        <div class="container">
-            <div class="row no-gutters">
-                <div class="col-lg-6">
-                    <!-- Contact Form Start -->
-                    <div class="contact-form">
-                        <!-- Section Title Start -->
-                        <div class="section-title">
-                            <h2 class="text-anime-style-3" data-cursor="-opaque">Have any questions?</h2>
-                        </div>
-                        <!-- Section Title End -->
-                         
-                        <!-- Contact Form Start -->
-                        <form id="contactForm" action="#" method="POST" data-toggle="validator" class="wow fadeInUp">
+                        <div class="contact-form">
+                             <form id="contactForm" action="#" method="POST" data-toggle="validator" class="wow fadeInUp">
                             <div class="row">
                                 <div class="form-group col-md-6 mb-4">
                                     <input type="text" name="fname" class="form-control" id="fname" placeholder="First Name" required>
@@ -145,7 +66,7 @@
                                     <div class="help-block with-errors"></div>
                                 </div>
 
-                                <div class="form-group col-md-12 mb-5">
+                                <div class="form-group col-md-12 mb-4">
                                     <textarea name="message" class="form-control" id="message" rows="4" placeholder="Write Message..."></textarea>
                                     <div class="help-block with-errors"></div>
                                 </div>
@@ -156,27 +77,118 @@
                                         <div id="msgSubmit" class="h3 hidden"></div>
                                     </div>
                                 </div>
+                                <div class="contact-info-content pt-5">
+                                <div class="follow-us">
+                                    <h3 class="text-anime-style-3" data-cursor="-opaque">
+                                        Follow Us
+                                    </h3>
+                                    <ul class="d-flex gap-3">
+                                        <li>
+                                            <a href="#">
+                                                <i class="fa-brands fa-facebook-f"></i>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                <i class="fa-brands fa-instagram"></i>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                <i class="fa-brands fa-x-twitter"></i>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
                             </div>
                         </form>
-                        <!-- Contact Form End -->
+                        </div>
+                        
                     </div>
-                    <!-- Contact Form End -->
-                </div>
+                    <div class="col-lg-5 col-md-6 col-sm-12">
+                        <div class="contact-image">
+                            <div class="profile-image">
+                                <img src="<?= base_url('public/assets/template/images/contact-us.png')?>" alt="">
+                            </div>
+                            <div class="quick-contact">
+                                <div class="row">
+                                    <div class="quick-box d-flex align-items-center gap-4 mt-4">
+                                        <div class="contactIcon">
+                                            <img src="<?=base_url('public/assets/template/images/icon-phone.svg')?>" >
+                                        </div>
+                                        <div>
+                                            <span>Contact Number</span>
+                                            <a href="tel:+918129394980">(+91) 812 939 4980</a>
+                                        </div>
+                                    </div>
 
-                <!-- Google Map Start -->
-                <div class="col-lg-6">
-                    <!-- Google Map Iframe Start -->
-                    <div class="google-map-iframe">
-
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7849.922289844222!2d76.245572!3d10.344992!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba7f7eec402ff03%3A0xf66bb33a88c3fd51!2sSports%20Injury%20%26%20Joint%20Preservation%20Clinic!5e0!3m2!1sen!2sin!4v1761887357955!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                                   
+                                    <div class="quick-box d-flex align-items-center gap-4 mt-2">
+                                        <div class="contactIcon">
+                                            <img src="<?=base_url('public/assets/template/images/icon-mail.svg')?>" >
+                                        </div>
+                                        <div>
+                                            <span>Mail Us</span>
+                                            <a href="mailto:drsudhanofficial.com">drsudhanofficial.com</a>
+                                        </div>
+                                    </div>
+                                    
+                                    <div class="quick-box d-flex align-items-center gap-4 mt-2">
+                                        <div class="contactIcon">
+                                            <img src="<?=base_url('public/assets/template/images/icon-location.svg')?>" >
+                                        </div>
+                                        <div>
+                                            <span>Location</span>
+                                            9/38, Pulinchode, PO, Potta - Moonupeedika Rd, near pulinchode bus stop, <br>Pullur, Irinjalakuda, Kerala 680683
+                                        </div>
+                                    </div>
+                                    
+                                </div>
+                            </div>
+                            
+                        </div>
                     </div>
-                    <!-- Google Map Iframe End -->
+                        
                 </div>
-                <!-- Google Map End -->
             </div>
         </div>
     </div>
-    <!-- Contact Form End -->
+    <!-- Page Contact Us End -->
+
+
+
+    <section class="main-section">
+        <div class="container">
+            <div class="inner-section">
+                <div class="map-section">
+                    <div class="row d-flex align-items-center">
+                        <div class="col-lg-6 col-md-12 col-sm-12">
+                            <div class="map-content">
+                                  <div class="section-title">
+                                    <h2 class="text-anime-style-3 ps-5 mb-2" data-cursor="-opaque">  
+                                        Accessible Care across locations 
+                                    </h2>
+                                    <h3 class="wow fadeInUp"> Our Location & Accessibility  </h3>
+                                    
+                                    <p class="wow fadeInUp" data-wow-delay="0.2s">Dr.Ram Sudhan Subramaniyan expert orthopaedic care easily accessible to patients from various regions. Whether you're nearby, from another state, or traveling internationally, personalized support and advanced treatment options are available to ensure your journey to recovery is smooth, comfortable, and effective.</p>
+                                </div>
+                               
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-md-12 col-sm-12">
+                            <div class="map-content">
+                                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/jvectormap-next/jquery-jvectormap.css">
+
+                                <div id="world-map" style="width:100%;height:25em;"></div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
 
     <!-- Page Contact Us End -->
@@ -184,5 +196,59 @@
 
     
 </body>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/jvectormap-next/jquery-jvectormap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/jvectormap-content/world-mill.js"></script>
+<script>
+var markers = [
+    {
+        latLng: [10.345092167082514, 76.24562584417761],
+        name: 'Join Factory',
+        phone: '+91 812 939 4980',
+        address: 'Kerala, India',
+        logo: 'assets/images/logo.png',
+        mapUrl: 'https://maps.google.com/?q=10.345092167082514,76.24562584417761'
+    }
+];
 
+$('#world-map').vectorMap({
+    map: 'world_mill',
+    backgroundColor: 'transparent',
+    
+
+    markers: markers,
+
+    markerStyle: {
+        initial: {
+            width: '20px',
+            height: '20px',
+            fill: '#2563eb',
+            stroke: '#fff',
+            r: 10
+        }
+    },
+
+    onMarkerTipShow: function(event, label, index) {
+
+        var marker = markers[index];
+
+        label.html(
+            ' <div style="min-width:150px;padding:10px; margin:0; border-radius: 10px;background:#fff;color:#000;display:flex;justify-content: center;align-items: center;">'+
+                                    '<div style="width: 90px;">'+
+                                        '<img src="<?= base_url('public/assets/template/') ?>images/favicon.png" style="width:100%;margin-bottom:10px;">'+
+                                    '</div>'+
+                                    '<div style="">'+
+                                        '<span>Join Factory</span>'+
+                                        '<p class="mb-0" >Kochi, Kerala, India</p>'+
+                                        '<p class="mb-0" >+91 812 939 4980</p>'+
+                                    '</div>'+
+                                '</div>'
+        );
+    },
+
+    onMarkerClick: function(event, index) {
+        window.open(markers[index].mapUrl, '_blank');
+    }
+});
+</script>
 </html>
